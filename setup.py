38f7507572f3b6be66afda5bd735af54d0952ce2
Joe Kington <joferkington@gmail.com>
2015-03-04 13:39:24 -0600

    Fix broken install
    "python setup.py install" was broken due to the package not being included in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
     
 setup(name='las_reader',
-      # packages=["las_reader",],
+      packages=["las_reader",],
       entry_points={'console_scripts': ['las2excel = las_reader.las2excel:main']})
 
